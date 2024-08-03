e39ec490d077ae78bc6a7cb4608874c1f5405ae9
dankichi24 <dan24.yukito@gmail.com>
2024-08-03 13:56:00 +0900

    サインアップ、ログイン実装

---
+++ pages/components/Post.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <div className="bg-white shadow-md rounded p-4 mb-4">
+      <div className="mb-4">
+        <div className="flex items-center mb-2">
+          <img
+            className="w-10 h-10 rounded-full mr-2"
+            src="https://via.placeholder.com/150"
+            alt="User Avatar"
+          />
+          <div>
+            <h2 className="font-semibold text-md">dankichi</h2>
+            <p className="text-gray-500 text-sm">08/01 19:51</p>
+          </div>
+        </div>
+        <p className="text-gray-700">はじめての投稿です。</p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
